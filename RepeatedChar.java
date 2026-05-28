6ce5f27c8866e5ba92383471dd9dbed53758adb8
Bharath <bharath23695a0508@gmail.com>
2026-05-28 15:26:13 +0530

    Repeated Character in a Given String

---
+++ RepeatedChar.java
@@ -0,0 +1,14 @@
+class RepeatedChar {
+    public static void main(String[] args) {
+       String s="javadev";
+       for(int i=0;i<s.length();i++)
+       {
+           char ch=s.charAt(i);
+           if(s.indexOf(ch)!=s.lastIndexOf(ch))
+           {
+               System.out.print(ch+" ");
+           }
+       }
+    }
+    
+}
